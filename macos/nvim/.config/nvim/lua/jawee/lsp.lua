6c242ea7444fbb3857d7ffdb61398c04e06b76d2
Andreas Olsson <olsson.andreas@gmail.com>
2022-02-15 15:50:57 +0100

    macos lsp.lua cleanup

--- macos/nvim/.config/nvim/lua/jawee/lsp.lua
+++ macos/nvim/.config/nvim/lua/jawee/lsp.lua
@@ -3,33 +3,11 @@ local on_attach = function()
     print("Lsp Started")
 end
 
-require'lspconfig'.tsserver.setup{ 
-    capabilities = capabilities,
-    on_attach=on_attach 
-}
 
-require'lspconfig'.gopls.setup{
+local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls' }
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
     capabilities = capabilities,
-    on_attach = on_attach, 
-}
-require'lspconfig'.clangd.setup{
-    capabilities = capabilities,
-    on_attach=on_attach
-}
--- who even uses this?
-require'lspconfig'.rust_analyzer.setup{ 
-    capabilities = capabilities, 
-    on_attach=on_attach 
-}
-
-local opts = {
-    -- whether to highlight the currently hovered symbol
-    -- disable if your cpu usage is higher than you want it
-    -- or you just hate the highlight
-    -- default: true
-    highlight_hovered_item = true,
-
-    -- whether to show outline guides
-    -- default: true
-    show_guides = true,
-}
+  }
+end
