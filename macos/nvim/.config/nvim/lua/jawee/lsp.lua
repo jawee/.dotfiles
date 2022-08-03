394179510f2ba6320985eaa86a5d57b11c210910
Andreas Olsson <olsson.andreas@gmail.com>
2022-08-03 15:26:23 +0200

    macos: fixed cmp autocomplete

--- macos/nvim/.config/nvim/lua/jawee/lsp.lua
+++ macos/nvim/.config/nvim/lua/jawee/lsp.lua
@@ -3,6 +3,7 @@ local on_attach = function()
     print("Lsp Started")
 end
 
+-- omnisharp
 local pid = vim.fn.getpid()
 local omnisharp_bin = "/Users/figge/.local/omnisharp/omnisharp"
 require'lspconfig'.omnisharp.setup{
@@ -11,6 +12,7 @@ require'lspconfig'.omnisharp.setup{
     capabilities = capabilities,
 }
 
+-- rest
 local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls', 'bashls'}
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
@@ -18,3 +20,29 @@ for _, lsp in pairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- lua
+require'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
