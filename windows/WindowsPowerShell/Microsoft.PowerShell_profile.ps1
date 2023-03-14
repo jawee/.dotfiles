a690d80e254c27b2602e8e46283e0854b7ad9822
Andreas Olsson <andreas.olsson@eastcoast.se>
2023-03-14 06:52:17 +0100

    windows things

---
+++ windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,42 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+
+function GoToWork {
+    GoToPath -Path C:\work\
+    # Set-Location C:\work\
+}
+
+function GoToPersonal {
+    GoToPath -Path C:\personal\
+    # Set-Location C:\personal\
+}
+
+# function GoToPath {
+#     param([String]$Path)
+#     Set-Location $path
+# }
+
+function GoToPath([string]$Path) {
+    Set-Location $Path
+}
+
+function OpenInVS() {
+    $file = Get-ChildItem *.sln | Select-Object -ExpandProperty Name
+    Invoke-expression ".\$file"
+}
+
+Set-Alias vs -value OpenInVS
+
+Set-Alias work -value GoToWork
+Set-Alias personal -value GoToPersonal
+
+Set-Alias ll ls
+
+Set-Alias open explorer.exe
+
+Set-PSReadlineKeyHandler -Chord Ctrl+f -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('work')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+$env:Path += ";C:\Users\andreaso\bin"
