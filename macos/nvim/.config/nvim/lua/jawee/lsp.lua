50d8d05ed530eefcc4c5843173a0c1023e893549
Andreas Olsson <olsson.andreas@gmail.com>
2023-03-19 13:33:23 +0100

    remove omnisharp

--- macos/nvim/.config/nvim/lua/jawee/lsp.lua
+++ macos/nvim/.config/nvim/lua/jawee/lsp.lua
@@ -3,15 +3,6 @@ local on_attach = function()
     print("Lsp Started")
 end
 
--- omnisharp
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/Users/figge/.local/omnisharp/omnisharp"
-require'lspconfig'.omnisharp.setup{
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) },
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
 -- rest
 local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls', 'bashls'}
 for _, lsp in pairs(servers) do
