91e5b712dea7f7c7101b84d8038ca5ced8edb153
Andreas Olsson <olsson.andreas@gmail.com>
2023-04-16 08:14:57 +0200

    removed omnisharp

--- wsl-ubuntu/nvim/.config/nvim/lua/jawee/lsp.lua
+++ wsl-ubuntu/nvim/.config/nvim/lua/jawee/lsp.lua
@@ -3,14 +3,6 @@ local on_attach = function()
     print("Lsp Started")
 end
 
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/home/figge/.local/omnisharp/OmniSharp"
-require'lspconfig'.omnisharp.setup{
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) },
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
-
 local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
