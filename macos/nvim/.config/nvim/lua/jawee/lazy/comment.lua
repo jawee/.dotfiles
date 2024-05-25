fc0b225115728c491af9d8806ddffa253ffdd4d4
Andreas Olsson <olsson.andreas@gmail.com>
2024-05-25 15:17:45 +0200

    change comment plugin

--- macos/nvim/.config/nvim/lua/jawee/lazy/comment.lua
+++ macos/nvim/.config/nvim/lua/jawee/lazy/comment.lua
@@ -1,6 +1,6 @@
 return {
-    "numToStr/Comment.nvim",
-    config = function()
-        require("Comment").setup()
-    end
+  "folke/ts-comments.nvim",
+  opts = {},
+  event = "VeryLazy",
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
 }
