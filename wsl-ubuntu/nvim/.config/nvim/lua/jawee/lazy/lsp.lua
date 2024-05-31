880e84030709fa999e50b9afde81c24cf707f16e
Andreas Olsson <olsson.andreas@gmail.com>
2024-05-31 07:13:03 +0200

    lsp lua update

--- wsl-ubuntu/nvim/.config/nvim/lua/jawee/lazy/lsp.lua
+++ wsl-ubuntu/nvim/.config/nvim/lua/jawee/lazy/lsp.lua
@@ -2,9 +2,12 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         'j-hui/fidget.nvim',
+        'folke/neodev.nvim',
+        'Decodetalkers/csharpls-extended-lsp.nvim',
     },
     config = function ()
         require("fidget").setup({})
+        require("neodev").setup({})
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
@@ -12,12 +15,20 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities())
         -- rest
-        local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls', 'bashls', 'ocamllsp', 'csharp_ls', 'lua_ls'}
+        local servers = { 'tsserver', 'rust_analyzer', 'clangd', 'gopls', 'bashls', 'ocamllsp', 'lua_ls'}
         for _, lsp in pairs(servers) do
             require('lspconfig')[lsp].setup {
                 capabilities = capabilities,
             }
         end
+
+        require('lspconfig').csharp_ls.setup({
+				capabilities = capabilities,
+				handlers = {
+					["textDocument/definition"] = require("csharpls_extended").handler,
+					["textDocument/typeDefinition"] = require("csharpls_extended").handler,
+				},
+			})
 
         vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(e)
@@ -31,8 +42,8 @@ return {
             vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
             vim.keymap.set("n", "<leader>vsd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "<leader>vn", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "<leader>vp", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<leader>vn", function() vim.diagnostic.jump({count = 1}) end, opts)
+            vim.keymap.set("n", "<leader>vp", function() vim.diagnostic.jump({count = -1}) end, opts)
             vim.keymap.set("n", "<leader>vfo", function() vim.lsp.buf.formatting() end, opts)
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end,
@@ -40,5 +51,3 @@ return {
     end
 
 }
-
-
