d4f623a25d94c250b5aaae0b9eececa05eeb0e78
Andreas Olsson <olsson.andreas@gmail.com>
2024-05-31 07:15:57 +0200

    trouble update

--- macos/nvim/.config/nvim/lua/jawee/lazy/trouble.lua
+++ macos/nvim/.config/nvim/lua/jawee/lazy/trouble.lua
@@ -1,21 +1,11 @@
 return {
     "folke/trouble.nvim",
     config = function()
-        require("trouble").setup({
-            icons = false,
-        })
+        require("trouble").setup({})
 
-        vim.keymap.set("n", "<leader>tt", function()
-            require("trouble").toggle()
-        end)
-
-        vim.keymap.set("n", "[t", function()
-            require("trouble").next({skip_groups = true, jump = true});
-        end)
-
-        vim.keymap.set("n", "]t", function()
-            require("trouble").previous({skip_groups = true, jump = true});
-        end)
+        vim.keymap.set("n", "<leader>t", "<cmd>Trouble diagnostics toggle<cr>")
+        vim.keymap.set("n", "[t", "<cmd>Trouble diagnostics next<cr>")
+        vim.keymap.set("n", "]t", "<cmd>Trouble diagnostics prev<cr>")
     end
 }
 
