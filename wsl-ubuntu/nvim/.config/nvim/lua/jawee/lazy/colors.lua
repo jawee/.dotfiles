c2389a54d5b675ba725ba7c8a333ffadd3eb9503
Andreas Olsson <olsson.andreas@gmail.com>
2024-06-04 07:41:02 +0200

    colors update

--- wsl-ubuntu/nvim/.config/nvim/lua/jawee/lazy/colors.lua
+++ wsl-ubuntu/nvim/.config/nvim/lua/jawee/lazy/colors.lua
@@ -34,5 +34,17 @@ return {
                 terminal_colors = true,
             })
         end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+        end
     }
 }
